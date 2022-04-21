81647cabd3788bbb7c9741ed330477fe9e8b943c
dnyaneshwar469 <dnyaneshwar.londhepatil@penpenny.com>
2022-04-21 18:18:20 +0530

    pipe tute in angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('serverForm') submitForm: NgForm;
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+
+  filteredStatus = '';
+
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
+
+  addNewServer = ''
+
+  onAddServer(){
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    })
+  }
+
+  onSubmit(){
+    // console.log(this.submitForm);
+    this.servers.push({
+      'instanceType': this.submitForm.value.instanceName,
+      'name' : this.submitForm.value.serverName,
+      'status': this.submitForm.value.status,
+      // 'started': this.submitForm.value.startingDate
+      'started' : new Date()
+    })
+  }
+}
